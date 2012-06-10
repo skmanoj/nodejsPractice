ba40493e2870223c9fa0a70b673429a1c8dbdcd8
Manoj Singaperumal <skmanoj1988@gmail.com>
2012-06-11 01:13:33 +0530

    intermediate request handling

---
+++ routingRequests2/requestHandlers.js
@@ -0,0 +1,10 @@
+function start()
+{
+  console.log('start called in requestHandlers');
+}
+function upload()
+{
+  console.log('upload called in requestHandlers');
+}
+exports.start=start;
+exports.upload=upload;
